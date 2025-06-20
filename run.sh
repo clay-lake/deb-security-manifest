2c329d46bee11bea0552a4541477d2d023770ad5
Adrian Clay Lake <clay.lake@canonical.com>
2025-06-20 17:57:37 +0200

    feat: update output path and fix help bug

--- run.sh
+++ run.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-OUTPUT_PATH="${1:-$CRAFT_PRIME/usr/share/rocks/output.txt}"
-DPKG_ADMINDIR="${2:-CRAFT_PRIME/var/lib/dpkg}"
-
 if [[ "$1" == "--help" ]]; then
   echo "Usage: $0 [output_path] [dpkg_admindir]"
   echo "  output_path: Optional. Path to the output file. Defaults to '$OUTPUT_PATH'."
   echo "  dpkg_admindir: Optional. Path to the dpkg admin directory. Defaults to '$DPKG_ADMINDIR'."
   exit 0
 fi
+
+OUTPUT_PATH="${1:-$CRAFT_PRIME/usr/share/rocks/dpkg.query}"
+DPKG_ADMINDIR="${2:-CRAFT_PRIME/var/lib/dpkg}"
 
 mkdir -p "$(dirname "$OUTPUT_PATH")"
 
